b1ae305019b8c19bb38004d7a0c08127d61b03d2
Arifuzzaman Tusar <arifuzzamantusar50@gmail.com>
2021-11-21 03:01:37 +0600

    Update connect.php

--- user/extension/connect.php
+++ user/extension/connect.php
@@ -1,8 +1,8 @@
 <?php
     $db_host = "66.45.250.53";
-	$db_user = "abolirez_soft";
+	$db_user = "arifuzza_abolirez_soft";
 	$db_pass = "abolirez_soft";
-	$db_name = "abolirez_soft";
+	$db_name = "arifuzza_abolirez_soft";
 	
 	$con = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 	if(mysqli_connect_error()){
